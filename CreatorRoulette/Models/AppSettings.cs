ad6aab724540762a96131c2e86976396efcfad5c
Rogelio Gomez <rxgerg@hotmail.com>
2026-07-08 15:00:55 -0600

    Add editable app settings

---
+++ CreatorRoulette/Models/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatorRoulette.Models
+{
+    public class AppSettings
+    {
+        public string AppTitle { get; set; } = "Creator Roulette";
+        public string CommunityName { get; set; } = "Mi comunidad";
+        public string SpinRoleButtonText { get; set; } = "Girar rol";
+        public string SpinCharacterButtonText { get; set; } = "Girar personaje";
+        public string SpinChallengeButtonText { get; set; } = "Girar reto";
+        public int SpinDurationMilliseconds { get; set; } = 2500;
+        public bool EnableSounds { get; set; } = true;
+    }
+}
---
+++ CreatorRoulette/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using CreatorRoulette.Models;
+using CreatorRoulette.Services;
+
+namespace CreatorRoulette.Views;
+
+public partial class SettingsWindow : Window
+{
+    private readonly SettingsService _settingsService;
+    private AppSettings _settings;
+
+    public SettingsWindow()
+    {
+        InitializeComponent();
+
+        _settingsService = new SettingsService();
+        _settings = _settingsService.Load();
+
+        LoadSettingsIntoForm();
+    }
+
+    private void LoadSettingsIntoForm()
+    {
+        AppTitleTextBox.Text = _settings.AppTitle;
+        CommunityNameTextBox.Text = _settings.CommunityName;
+        SpinRoleButtonTextBox.Text = _settings.SpinRoleButtonText;
+        SpinCharacterButtonTextBox.Text = _settings.SpinCharacterButtonText;
+        SpinChallengeButtonTextBox.Text = _settings.SpinChallengeButtonText;
+        EnableSoundsCheckBox.IsChecked = _settings.EnableSounds;
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        _settings.AppTitle = AppTitleTextBox.Text;
+        _settings.CommunityName = CommunityNameTextBox.Text;
+        _settings.SpinRoleButtonText = SpinRoleButtonTextBox.Text;
+        _settings.SpinCharacterButtonText = SpinCharacterButtonTextBox.Text;
+        _settings.SpinChallengeButtonText = SpinChallengeButtonTextBox.Text;
+        _settings.EnableSounds = EnableSoundsCheckBox.IsChecked == true;
+
+        _settingsService.Save(_settings);
+
+        MessageBox.Show("Configuración guardada correctamente.", "Creator Roulette");
+        Close();
+    }
+}
